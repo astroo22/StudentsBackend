fe1b064c6ea244e78caf2f402cc8bcb8bf23e758
astroo22 <astroo22@gmail.com>
2023-07-29 16:32:17 -0500

    attempts

--- .platform/nginx/conf.d
+++ .platform/nginx/conf.d
@@ -32,7 +32,7 @@ http {
         root         /usr/share/nginx/html;
 
         location / {
-            proxy_pass http://127.0.0.1:8080; # Modify this line to pass requests to your Go application
+            proxy_pass http://127.0.0.1:8080;
             proxy_http_version 1.1;
             proxy_set_header Upgrade $http_upgrade;
             proxy_set_header Connection 'upgrade';
