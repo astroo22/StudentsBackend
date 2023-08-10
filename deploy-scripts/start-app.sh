d966fc4d437ee72d66d7cf9ca0247244ea5a53cd
astroo22 <astroo22@gmail.com>
2023-08-09 19:24:47 -0500

    updates

--- deploy-scripts/start-app.sh
+++ deploy-scripts/start-app.sh
@@ -4,17 +4,9 @@ LOGFILE="/var/log/start-app.log"
 
 exec 2>>$LOGFILE
 
-export APP_ENV=prod
-
-# If the 'students' process is already running, kill it
-if pgrep students > /dev/null; then
-    echo "Students service is currently running. Stopping..." >> $LOGFILE
-    pkill students
-    sleep 2 # give second to allow it to shutdown
-fi
-
 echo "Starting the students service..." >> $LOGFILE
-/var/www/backend/bin/students &
+systemctl start students.service
+
 if [ $? -eq 0 ]; then
     echo "Successfully started the students service." >> $LOGFILE
     exit 0
--- deploy-scripts/stop-app.sh
+++ deploy-scripts/stop-app.sh
@@ -4,16 +4,13 @@ LOGFILE="/var/log/stop-app.log"
 
 exec 2>>$LOGFILE
 
-if pgrep students > /dev/null; then
-    echo "Stopping the students service..." >> $LOGFILE
-    pkill students
-    if [ $? -eq 0 ]; then
-        echo "Successfully stopped the students service." >> $LOGFILE
-    else
-        echo "Failed to stop the students service." >> $LOGFILE
-    fi
-else
-    echo "Students service is not running. Nothing to stop." >> $LOGFILE
-fi
+echo "Stopping the students service..." >> $LOGFILE
+systemctl stop backend.service
 
-exit 0
+if [ $? -eq 0 ]; then
+    echo "Successfully stopped the students service." >> $LOGFILE
+    exit 0
+else
+    echo "Failed to stop the students service." >> $LOGFILE
+    exit 1
+fi
