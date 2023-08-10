49812f861c4ffb57c92dd765b3eeac568779f342
astroo22 <astroo22@gmail.com>
2023-08-09 19:31:24 -0500

    incorrect naming fixed

--- deploy-scripts/start-app.sh
+++ deploy-scripts/start-app.sh
@@ -4,13 +4,13 @@ LOGFILE="/var/log/start-app.log"
 
 exec 2>>$LOGFILE
 
-echo "Starting the students service..." >> $LOGFILE
-systemctl start students.service
+echo "Starting the backend service..." >> $LOGFILE
+systemctl start backend.service
 
 if [ $? -eq 0 ]; then
-    echo "Successfully started the students service." >> $LOGFILE
+    echo "Successfully started the backend service." >> $LOGFILE
     exit 0
 else
-    echo "Failed to start the students service." >> $LOGFILE
+    echo "Failed to start the backend service." >> $LOGFILE
     exit 1
 fi
